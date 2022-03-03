fde6d97cc6b0ce814b33fd0f889f21e7ce0435c2
Brijesh <dobariyabrijesh07@gmail.com>
2022-03-03 19:10:56 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <div>Commenty</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/MainContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+
+function MainContainer() {
+  return (
+    <div>
+      <div className="main-container">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default MainContainer;
