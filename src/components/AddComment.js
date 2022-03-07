a4d45de753f358ce48186a94932b8d5392846eab
dobariyabrijesh07 <dobariyabrijesh07@gmail.com>
2022-03-07 23:46:24 +0530

    commit

---
+++ src/components/AddComment.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AddComment() {
+	return (
+		<div>
+			<div className="add-comment">
+				<input type="text" />
+			</div>
+		</div>
+	);
+}
+
+export default AddComment;
--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -1,48 +1,52 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { FaRegHeart, FaShare } from "react-icons/fa";
+import { FaShare } from "react-icons/fa";
+import { BsHeartFill } from "react-icons/bs";
+import AddComment from "./AddComment";
 
 function CommentsContainer() {
-  const [comments, setComments] = useState([]);
+	const [comments, setComments] = useState([]);
 
-  const getComments = () => {
-    axios
-      .get("https://62207dfdce99a7de195b3ec5.mockapi.io/commenty")
-      .then((res) => setComments(res.data))
-      .catch((err) => alert(err));
-  };
+	const getComments = () => {
+		axios
+			.get("https://62207dfdce99a7de195b3ec5.mockapi.io/commenty")
+			.then((res) => setComments(res.data))
+			.catch((err) => alert(err));
+	};
 
-  useEffect(() => {
-    getComments();
-  }, []);
+	useEffect(() => {
+		getComments();
+	}, []);
 
-  return (
-    <div>
-      <div className="comments-container">
-        {comments.map((comment) => {
-          return (
-            <div className="comment">
-              <div className="comment-text">{comment.text}</div>
-              <div className="action-icons">
-                <FaRegHeart className={comment.like ? "like" : ""} />
-                <FaShare />
-              </div>
-            </div>
-          );
-        })}
-        <div className="comment">
-          <div className="comment-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-            consectetur.
-          </div>
-          <div className="action-icons">
-            <FaRegHeart />
-            <FaShare />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="comments-container">
+				{comments.map((comment) => {
+					return (
+						<div className="comment">
+							<div className="comment-text">{comment.text}</div>
+							<div className="action-icons">
+								<BsHeartFill className={comment.like ? "like" : "unlike"} />
+								<FaShare />
+							</div>
+						</div>
+					);
+				})}
+				<div className="comment">
+					<div className="comment-text">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+						consectetur.
+					</div>
+					<div className="action-icons">
+						<BsHeartFill />
+						<FaShare />
+					</div>
+				</div>
+
+				<AddComment />
+			</div>
+		</div>
+	);
 }
 
 export default CommentsContainer;
